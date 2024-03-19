eb44a26c2a1ab05b9c54634228140a37b7f237a8
achen5401 <achen5401@gmail.com>
2024-03-19 12:57:31 -0400

    .

--- frontend/src/apis/store-api.js
+++ frontend/src/apis/store-api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/api', // Replace this with your backend API base URL
-    //baseURL: 'https://ratethis-backend.onrender.com/api',
+    //baseURL: 'http://localhost:4000/api', // Replace this with your backend API base URL
+    baseURL: 'https://ratethis-backend.onrender.com/api',
     timeout: 10000, // Adjust timeout as needed
 });
 
